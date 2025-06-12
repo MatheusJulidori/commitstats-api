f2424471e70e331c7445a799e78728246f35e8c1
Matheus Henrique Braga Julidori <matheus.julidori@inatel.br>
2025-06-12 18:41:55 -0300

    Folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("CommitStats API started 🚀");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
